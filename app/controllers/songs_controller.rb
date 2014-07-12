f6fac36b5074fefa22aab77fb71adfa4e1637dc7
mzp <mzpppp@gmail.com>
2014-07-12 23:11:13 +0900

    add url field to song info

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,8 @@
 class SongsController < ApplicationController
   protect_from_forgery except: :add
   def add
-    path = write_file request.body
-    save_info path
+    info = write_file request.body
+    save_info info
   end
 
   def index
@@ -17,14 +17,17 @@ class SongsController < ApplicationController
     filename = Time.now.strftime("%Y%m%d%H%M%S%L.m4a")
     path = File.expand_path(filename, dir)
     IO.binwrite(path, io.read)
-    path
+
+    url =  "http://#{request.host}:#{request.port}/music/#{filename}"
+
+    { path: path, url: url }
   end
 
-  def save_info(path)
+  def save_info(info)
     count = Redis.current.incr "song-id"
     id = "song:#{count}"
-    Rails.logger.debug "Add '#{id}' with { path: #{path.inspect} }"
-    Redis.current.set id, { path: path }
+    Rails.logger.debug "Add '#{id}' with #{info.inspect}"
+    Redis.current.set id, info
     Redis.current.rpush "playlist", id
   end
 end
