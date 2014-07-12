310341bfe1745a9c0d536584563c990f8fcb7f6f
mzp <mzpppp@gmail.com>
2014-07-12 20:33:10 +0900

    bump up to Rails 4.1.0

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-AirHack::Application.initialize!
+Rails.application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-AirHack::Application.config.session_store :cookie_store, key: '_air-hack_session'
+Rails.application.config.session_store :cookie_store, key: '_air-hack_session'
