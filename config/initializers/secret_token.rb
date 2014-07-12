2c85ab367aff2f405b0f003260d9d910e06c42f5
mzp <mzpppp@gmail.com>
2014-07-12 18:38:59 +0900

    bundle exec rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirHack::Application.config.secret_key_base = 'aa92af46fa7f192103960ce2b3eaf804669bec8630af5f58d6ba0236f349dc4a4eac17b1cfa3cfdccbda6123dd05e1bdfad0129e0b9b85dc715b8895095b28e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AirHack::Application.config.session_store :cookie_store, key: '_air-hack_session'
