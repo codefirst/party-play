3c84e147793700de8af9cb06e5d78f0becd43302
mzp <mzpppp@gmail.com>
2014-07-12 21:13:12 +0900

    push song to playlist

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,8 @@
 class SongsController < ApplicationController
   protect_from_forgery except: :add
   def add
-    filename = Time.now.strftime("%Y%m%d%H%M%S%L.m4a")
-    path = File.expand_path(filename, dir)
-    IO.binwrite(path, request.body.read)
+    path = write_file request.body
+    save_info path
   end
 
   def index
@@ -12,5 +11,20 @@ class SongsController < ApplicationController
   private
   def dir
     File.expand_path("public/music", Rails.root).tap(&FileUtils.method(:mkdir_p))
+  end
+
+  def write_file(io)
+    filename = Time.now.strftime("%Y%m%d%H%M%S%L.m4a")
+    path = File.expand_path(filename, dir)
+    IO.binwrite(path, io.read)
+    path
+  end
+
+  def save_info(path)
+    count = Redis.current.incr "song-id"
+    id = "song:#{count}"
+    Rails.logger.debug "Add '#{id}' with { path: #{path.inspect} }"
+    Redis.current.set id, { path: path }
+    Redis.current.rpush "playlist", id
   end
 end
