18d8b7e26340c741736d575a6986e74783499718
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-07-13 09:51:54 +0900

    Added: redis connection environment variables

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
 require 'redis'
-Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
+REDIS_HOST = ENV['REDIS_HOST'] || '127.0.0.1'
+REDIS_PORT = ENV['REDIS_PORT'] || '6379'
+Redis.current = Redis.new(host: REDIS_HOST, port: REDIS_PORT.to_i)
